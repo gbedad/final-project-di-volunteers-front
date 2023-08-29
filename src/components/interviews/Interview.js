3c116d83c34709d1c86174bb869f5b02577c3bec
gbedad <gerald.berrebi@gmail.com>
2023-08-29 16:04:34 +0300

    Set date today for interview

--- src/components/interviews/Interview.js
+++ src/components/interviews/Interview.js
@@ -324,8 +324,7 @@ const FormInterviewComponent = ({ userId }) => {
                     size="small"
                     label="Date de l'entretien"
                     type="date"
-                    defaultValue={date.now()}
-                    value={interview.date}
+                    value={interview.date || new Date()}
                     onChange={(e) => handleDateChange(e.target.value, index)}
                     InputLabelProps={{
                       shrink: true,
