53b1d18e5459430db0413fc6b5932f84a268b705
gbedad <gerald.berrebi@gmail.com>
2023-11-24 14:35:16 +0200

    set toast error in register for existing email

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -21,6 +21,9 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Input from 'react-phone-number-input/input';
 import CustomPhoneNumber from '../components/phone-numbers/PhoneNumber';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -77,6 +80,16 @@ const RegisterForm = ({ mission }) => {
       navigate('/login');
     } catch (error) {
       console.error(error); // Handle error here
+      if (error.response && error.response.status === 409) {
+        // Assuming 409 is the status code for email already existing
+        toast.error('Email already exists. Please use a different email.', {
+          position: 'top-center',
+        });
+      } else {
+        toast.error('An error occurred. Please try again later.', {
+          position: 'top-center',
+        });
+      }
     }
   };
 
@@ -164,67 +177,69 @@ const RegisterForm = ({ mission }) => {
     //     Register
     //   </Button>
     // </form>
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}>
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up to mission {propsData}
-          </Typography>
+    <>
+      <ToastContainer />
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
           <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="first_name"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                  value={first_name}
-                  onChange={(e) => setFirstName(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="last_name"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                  value={last_name}
-                  onChange={(e) => setLastName(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                {/* <TextField
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}>
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up to mission {propsData}
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="first_name"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                    value={first_name}
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="last_name"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="family-name"
+                    value={last_name}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  {/* <TextField
                   autoComplete="phone-number"
                   name="phone"
                   required
@@ -235,85 +250,86 @@ const RegisterForm = ({ mission }) => {
                   value={phone}
                   onChange={(e) => setPhone(e.target.value)}
                 /> */}
-                <Input
-                  international
-                  withCountryCallingCode
-                  defaultCountry="FR"
-                  placeholder="Phone number"
-                  value={phone}
-                  onChange={setPhone}
-                  style={{ innerHeight: '40px' }}
-                  inputComponent={CustomPhoneNumber}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    value={birth_date}
-                    onChange={(newValue) => setBirthDate(newValue)}
-                    required
-                    id="birthdate"
-                    label="Birth Date"
+                  <Input
+                    international
+                    withCountryCallingCode
+                    defaultCountry="FR"
+                    placeholder="Phone number"
+                    value={phone}
+                    onChange={setPhone}
+                    style={{ innerHeight: '40px' }}
+                    inputComponent={CustomPhoneNumber}
                   />
-                </LocalizationProvider>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      value={birth_date}
+                      onChange={(newValue) => setBirthDate(newValue)}
+                      required
+                      id="birthdate"
+                      label="Birth Date"
+                    />
+                  </LocalizationProvider>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextareaAutosize
+                    required
+                    id="message"
+                    label="Motivation"
+                    name="message"
+                    aria-label="minimum height"
+                    minRows={5}
+                    placeholder="Motivation"
+                    style={{ width: 400 }}
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox value="allowExtraEmails" color="primary" />
+                    }
+                    label="Click here to indicate that you have read and agree to the terms presented in the Terms and Conditions agreement."
+                  />
+                </Grid>
               </Grid>
 
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}>
+                Sign Up
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link href="/login" variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <TextareaAutosize
-                  required
-                  id="message"
-                  label="Motivation"
-                  name="message"
-                  aria-label="minimum height"
-                  minRows={5}
-                  placeholder="Motivation"
-                  style={{ width: 400 }}
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox value="allowExtraEmails" color="primary" />
-                  }
-                  label="Click here to indicate that you have read and agree to the terms presented in the Terms and Conditions agreement."
-                />
-              </Grid>
-            </Grid>
-
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}>
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/login" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
+            </Box>
           </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
+          <Copyright sx={{ mt: 5 }} />
+        </Container>
+      </ThemeProvider>
+    </>
   );
 };
 
