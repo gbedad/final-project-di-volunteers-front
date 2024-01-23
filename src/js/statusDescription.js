7b8bbd7cc869628c6a42443db667fbf9cde4587d
gbedad <gerald.berrebi@gmail.com>
2024-01-23 17:35:35 +0200

    Fix bugs"

--- src/js/statusDescription.js
+++ src/js/statusDescription.js
@@ -1,16 +1,16 @@
 export function shortDescription(key) {
   switch (key) {
-    case 'compte créé':
+    case 'Compte créé':
       return 'Votre compte est créé';
-    case 'à renseigner':
+    case 'A renseigner':
       return 'Nous aimerions en savoir plus';
-    case 'à interviewer':
+    case 'A interviewer':
       return 'Le moment est venu de s’entretenir';
-    case 'à finaliser':
+    case 'A finaliser':
       return 'Il reste à compléter votre dossier';
-    case 'validé':
+    case 'Validé':
       return 'Votre candidature est validée';
-    case 'déclinée':
+    case 'Déclinée':
       return `Malheureusement, votre candidature est déclinée`;
     default:
       return 'Votre compte est créé';
@@ -23,7 +23,7 @@ export function longDescription(key) {
       return 'Le candidat s’est enregistré sur l’application grâce aux données : mail, nom, prénom, date de naissance, texte de motivation';
     case 'A renseigner':
       return 'Après vérifications de base (s’agit-il d’une candidature farfelue, incohérente ou indésirable, le texte de motivation est-il très insuffisant, etc.), l’administrateur change le statut « Compte créé » en statut « A renseigner ». Ainsi, le candidat accède à la possibilité de renseigner toutes les informations complémentaires (disponibilités, documents) dont celles requises pour organiser un entretien.';
-    case 'à interviewer':
+    case 'A interviewer':
       return `L’administrateur passe le dossier du statut « A renseigner » au statut « A interviewer » dès qu’il juge les informations renseignées suffisante pour lancer un processus d’entretien(s).
         Pendant tout ce processus, que les entretiens soient planifiés ou pas, qu’ils soient réalisés ou pas, qu’il y ait un ou plusieurs entretiens, le candidat reste dans ce statut.`;
     case 'A finaliser':
@@ -31,7 +31,7 @@ export function longDescription(key) {
     case 'Validé':
       return `L’administrateur passe le dossier de « A finaliser » en « Validé » lorsque tous les documents sont chargés et complétés le cas échéant : pièce d’identité, extrait de casier judiciaire, CV et/ou diplômes, convention signée.
         Une candidature peut être validée sans le bénévole soit actif (il est alors soit en attente de l’allocation d’un élève, soit inactif pour d’autres raisons).`;
-    case 'déclinée':
+    case 'Déclinée':
       return `Malheureusement, votre candidature est déclinée.`;
     default:
       break;
