70390e0162237072d1181bf45ec9c2ffc3c7797a
gbedad <gerald.berrebi@gmail.com>
2024-04-13 15:53:17 +0300

    Direct to mission 63

--- src/components/navbar/AppBarMenu.js
+++ src/components/navbar/AppBarMenu.js
@@ -371,8 +371,7 @@ function ResponsiveAppBar() {
                 </MenuItem>
               ))}
               {!location.state || !location.state.userLogged ? (
-                <MenuItem
-                  onClick={() => navigate('/register', { state: '63' })}>
+                <MenuItem onClick={() => navigate('/register', { state: 63 })}>
                   <Typography textAlign="center">Créer un compte</Typography>
                 </MenuItem>
               ) : null}
