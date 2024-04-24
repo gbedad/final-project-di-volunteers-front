3dca86f535c25f15f1a28d0d802179dd246d5c6a
gbedad <gerald.berrebi@gmail.com>
2024-04-24 15:38:38 +0300

    check response in resetPassword

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -105,7 +105,7 @@ const ResetPassword = () => {
         const data = await res.json();
         console.log(data);
 
-        if (data.status === 201) {
+        if (res.status === 201) {
           setPassword('');
           setConfirmPassword('');
           // setMessage(true);
