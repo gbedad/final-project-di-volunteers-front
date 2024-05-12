1a6cd73b3c1439297036a667bdf3b72ee9b84ec8
gbedad <gerald.berrebi@gmail.com>
2024-05-12 21:42:49 +0300

    Correct bug at AuthProvider

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,3 +1,109 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+import { isTokenExpired } from './js/auth';
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+  const [isInitialLoad, setIsInitialLoad] = useState(true); // New state to track initial load
+
+  const checkTokenAndRedirect = (token) => {
+    if (isInitialLoad) {
+      setIsInitialLoad(false); // Update state to indicate the initial load is complete
+      return; // Skip redirection on initial load
+    }
+    if (!token || isTokenExpired(token)) {
+      logout();
+    } else {
+      setIsLoggedIn(true);
+      setToken(token);
+    }
+  };
+  const updateToken = (newToken) => {
+    setToken(newToken);
+  };
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    if (storedToken) {
+      setIsLoggedIn(true);
+      setToken(storedToken);
+      setIsInitialLoad(false); // Set initial load as complete if token is found
+    }
+  }, []);
+
+  const login = async (email, password) => {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_BASE_URL}/login`,
+        { email, password }
+      );
+      localStorage.setItem('token', response.data.token); // Assume the token is returned here
+      setIsLoggedIn(true);
+      setToken(response.data.token);
+      setUser(response.data.user); // Assuming response contains user data
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+    setToken(null);
+    setUser(null);
+    localStorage.clear(); // Clear all local storage
+    navigate('/login');
+  };
+
+  const refreshToken = async () => {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_BASE_URL}/refresh-token`,
+        { refreshToken: localStorage.getItem('refreshToken') }
+      );
+      localStorage.setItem('token', response.data.accessToken);
+      setToken(response.data.accessToken);
+    } catch (error) {
+      console.error(error);
+      logout(); // Logout if refresh fails
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentToken = localStorage.getItem('token');
+      checkTokenAndRedirect(currentToken);
+    }, 5 * 60 * 1000); // Refresh every 5 minutes
+
+    return () => clearInterval(interval);
+  }, [user]); // Depend on user state
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        token,
+        login,
+        logout,
+        updateToken,
+        refreshToken,
+        user,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthProvider };
+
 // import { createContext, useContext, useState, useEffect } from 'react';
 // import { useNavigate } from 'react-router-dom';
 // import axios from 'axios';
@@ -145,106 +251,3 @@
 // };
 
 // export { AuthContext, AuthProvider };
-
-import { createContext, useContext, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-
-import { isTokenExpired } from './js/auth';
-
-const AuthContext = createContext();
-
-export const useAuth = () => useContext(AuthContext);
-
-const AuthProvider = ({ children }) => {
-  const navigate = useNavigate();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [token, setToken] = useState(null);
-  const [user, setUser] = useState(null);
-  const [isInitialLoad, setIsInitialLoad] = useState(true); // New state to track initial load
-
-  const checkTokenAndRedirect = (token) => {
-    if (isInitialLoad) {
-      setIsInitialLoad(false); // Update state to indicate the initial load is complete
-      return; // Skip redirection on initial load
-    }
-    if (!token || isTokenExpired(token)) {
-      logout();
-    } else {
-      setIsLoggedIn(true);
-      setToken(token);
-    }
-  };
-
-  useEffect(() => {
-    const storedToken = localStorage.getItem('token');
-    if (storedToken) {
-      setIsLoggedIn(true);
-      setToken(storedToken);
-      setIsInitialLoad(false); // Set initial load as complete if token is found
-    }
-  }, []);
-
-  const login = async (email, password) => {
-    try {
-      const response = await axios.post(
-        `${process.env.REACT_APP_BASE_URL}/login`,
-        { email, password }
-      );
-      localStorage.setItem('token', response.data.token); // Assume the token is returned here
-      setIsLoggedIn(true);
-      setToken(response.data.token);
-      setUser(response.data.user); // Assuming response contains user data
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const logout = () => {
-    setIsLoggedIn(false);
-    setToken(null);
-    setUser(null);
-    localStorage.clear(); // Clear all local storage
-    navigate('/login');
-  };
-
-  const refreshToken = async () => {
-    try {
-      const response = await axios.post(
-        `${process.env.REACT_APP_BASE_URL}/refresh-token`,
-        { refreshToken: localStorage.getItem('refreshToken') }
-      );
-      localStorage.setItem('token', response.data.accessToken);
-      setToken(response.data.accessToken);
-    } catch (error) {
-      console.error(error);
-      logout(); // Logout if refresh fails
-    }
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const currentToken = localStorage.getItem('token');
-      checkTokenAndRedirect(currentToken);
-    }, 5 * 60 * 1000); // Refresh every 5 minutes
-
-    return () => clearInterval(interval);
-  }, [user]); // Depend on user state
-
-  return (
-    <AuthContext.Provider
-      value={{
-        isLoggedIn,
-        token,
-        login,
-        logout,
-
-        refreshToken,
-        user,
-      }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
-
-export { AuthContext, AuthProvider };
